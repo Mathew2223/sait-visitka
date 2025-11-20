2169b40cbf3a61f3bc7eac0d79b01be6e0b86160
Ситниковы <sitnikovmatvey2008@gmail.com>
2025-11-20 22:05:53 +0300

    fix: header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,36 @@
-import './Header.css'
-import logo from './assets/logo_header.png'
+import { useState } from 'react';
+import './Header.css';
+import logo from './assets/logo_header.png';
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
     return (
         <header className='header'>
             <div className='container'>
                 <div className="menu_row">
                     <img className='logotype' src={logo} alt="logo" />
-                    <nav className='nav-links'>
-                        <a className='menu-link' href="#program">Программа курса</a>
-                        <a className='menu-link' href="#price">Стоимость обучения</a>
-                        <a className='menu-link' href="#reviews">Отзывы</a>
-                        <a className='menu-link' href="#contacts">Контакты</a>
+                    
+                    {/* Гамбургер кнопка */}
+                    <button 
+                        className={`hamburger ${isMenuOpen ? 'hamburger--active' : ''}`}
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </button>
+
+                    {/* Навигация */}
+                    <nav className={`nav-links ${isMenuOpen ? 'nav-links--active' : ''}`}>
+                        <a className='menu-link' href="#program" onClick={() => setIsMenuOpen(false)}>Программа курса</a>
+                        <a className='menu-link' href="#price" onClick={() => setIsMenuOpen(false)}>Стоимость обучения</a>
+                        <a className='menu-link' href="#reviews" onClick={() => setIsMenuOpen(false)}>Отзывы</a>
+                        <a className='menu-link' href="#contacts" onClick={() => setIsMenuOpen(false)}>Контакты</a>
                     </nav>
+
+                    {/* Затемнение фона при открытом меню */}
+                    {isMenuOpen && <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>}
                 </div>
             </div>
         </header>
